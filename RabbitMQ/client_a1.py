571888f96d6f37cda64563dcbee1efda4d2a6ae7
Vibhu Dubey <vibhudubey97.a@gmail.com>
2023-02-19 04:52:22 +0530

    almost complete

--- RabbitMQ/client_a1.py
+++ RabbitMQ/client_a1.py
@@ -38,6 +38,30 @@ class Client(object):
 				print(f"{server.name} -> {server.addr}")
 
 			print("got it!")
+		elif props.type == "join-server":
+			print(str(body))
+		elif props.type == "leave-server":
+			print(str(body))
+		elif props.type == "get-articles":
+			article_list = server_pb2.ArticleList()
+			article_list.ParseFromString(body)
+
+			if article_list.status == False:
+				print("FAIL")
+			else:
+				article_list = server_pb2.ArticleList()
+				article_list.ParseFromString(body)
+				for article in article_list.articles:
+					artTag = "SPORTS" if (article.SPORTS) else "FASHION" if (article.FASHION) else "POLITICS"
+					print(artTag)
+					print(article.author)
+					print(f"{article.day}/{article.month}/{article.year}")
+					print(article.content)
+					print()
+		
+		elif props.type == "pub-article":
+			print(str(body))
+
 
 		
 
@@ -63,24 +87,186 @@ class Client(object):
 
 		return 0
 
-test_client = Client()
 
-# print(test_client.getServerList())
+	def joinServer(self):
+		if len(self.server_list) == 0:
+			print("No servers available")
+			return
+		# print("Se")
+		for i in range(len(self.server_list)):
+			print(f"ID: {i}")
+			print(self.server_list[i])
+			print("-----------------------------------")
+		
+		print("Enter the server ID you want to join: ")
+		server_id = int(input())
 
-# test_client.getServerList()
-# test_client.getServerList()
-# test_client.getServerList()
+		server_detail = server_pb2.ClientDetails()
+		server_detail.client_uuid = self.id
+		message = server_detail.SerializeToString()
 
-# while True:
-# 	test_client.getServerList()
-# 	a = 1
+		self.channel.basic_publish(
+			exchange="",
+			routing_key=self.server_list[server_id].addr,
+			properties=pika.BasicProperties(
+				reply_to=self.callback_queue,
+				correlation_id=self.id,
+				type="join-server",
+				# user_id=self.id,
+				),
+			body=message
+		)
+
+		self.connection.process_data_events(time_limit=None)
+
+
+	def leaveServer(self):
+		if len(self.server_list) == 0:
+			print("No servers available")
+			return
+		# print("Se")
+		for i in range(len(self.server_list)):
+			print(f"ID: {i}")
+			print(self.server_list[i])
+			print("-----------------------------------")
+		
+		print("Enter the server ID you want to leave: ")
+		server_id = int(input())
+
+		server_detail = server_pb2.ClientDetails()
+		server_detail.client_uuid = self.id
+		message = server_detail.SerializeToString()
+
+		self.channel.basic_publish(
+			exchange="",
+			routing_key=self.server_list[server_id].addr,
+			properties=pika.BasicProperties(
+				reply_to=self.callback_queue,
+				correlation_id=self.id,
+				type="leave-server",
+				# user_id=self.id,
+				),
+			body=message
+		)
+
+		self.connection.process_data_events(time_limit=None)
+
+
+	def getArticles(self):		
+		if len(self.server_list) == 0:
+			print("No servers available")
+			return
+		# print("Se")
+		for i in range(len(self.server_list)):
+			print(f"ID: {i}")
+			print(self.server_list[i])
+			print("-----------------------------------")
+		
+		print("Enter the server ID you want to get article from: ")
+		server_id = int(input())
+
+
+		tag = input("Article tag (sports, fashion, politics, else all): ").lower() 
+		# tag
+		req_article = server_pb2.RequestMessage(client_uuid=self.id)
+		if(tag == 'sports'):
+			req_article.SPORTS = True
+		elif(tag == 'fashion'):
+			req_article.FASHION = True 
+		elif(tag == 'politics'):
+			req_article.POLITICS = True 
+		else:
+			req_article.allTags = True
+		# author 
+		author = input("Author name (or blank):")
+		req_article.author = author
+		# date 
+		day = int(input("day: "))
+		month = int(input("month: "))
+		year = int(input("year: "))
+		print(day, month, year, author)
+		req_article.day = day
+		req_article.month = month
+		req_article.year = year
+
+		print(req_article)	
+		# Send Request
+
+		message = req_article.SerializeToString()
+
+		self.channel.basic_publish(
+			exchange="",
+			routing_key=self.server_list[server_id].addr,
+			properties=pika.BasicProperties(
+				reply_to=self.callback_queue,
+				correlation_id=self.id,
+				type="get-articles",
+				# user_id=self.id,
+				),
+			body=message
+		)
+
+		self.connection.process_data_events(time_limit=None)
+
+
+	def publishArticle(self):
+		if len(self.server_list) == 0:
+			print("No servers available")
+			return
+		# print("Se")
+		for i in range(len(self.server_list)):
+			print(f"ID: {i}")
+			print(self.server_list[i])
+			print("-----------------------------------")
+		
+		print("Enter the server ID you want to publish to: ")
+		server_id = int(input())
+
+
+		tag = input("Article tag (sports, fashion, politics): ").lower() 
+		author = input("Enter author name: ")
+		content = input("Enter content: ")
+		day = int(1)
+		month = int(1)
+		year = int(1)
+
+		pub_article = server_pb2.Article(author=author,content=content,day=day,month=month,year=year,client_uuid=self.id)
+		if(tag == 'sports'):
+			pub_article.SPORTS = True
+		elif(tag == 'fashion'):
+			pub_article.FASHION = True 
+		elif(tag == 'politics'):
+			pub_article.POLITICS = True 
+		else:
+			print("Invalid Tag")
+			return
+
+
+		# Call RPC
+		message = pub_article.SerializeToString()
+
+		self.channel.basic_publish(
+			exchange="",
+			routing_key=self.server_list[server_id].addr,
+			properties=pika.BasicProperties(
+				reply_to=self.callback_queue,
+				correlation_id=self.id,
+				type="pub-article",
+				# user_id=self.id,
+				),
+			body=message
+		)
+
+		self.connection.process_data_events(time_limit=None)
+
+	
 
 if __name__== '__main__':
 	client = Client()
 	ch = 'x'
 	while(ch != 'q'):
 		print("---------------####----------------")
-		print("Get server list (g) | Join Server (j) | Get Article (a) | Publish (p) | Quit (q):")
+		print("Get server list (g) | Join Server (j) | Leave Server (h) | Get Article (a) | Publish (p) | Quit (q):")
 		ch = input().lower()
 		if(ch == 'q'):
 			break 
@@ -88,6 +274,8 @@ if __name__== '__main__':
 			client.getServerList()
 		elif(ch == 'j'):
 			client.joinServer()
+		elif(ch == 'h'):
+			client.leaveServer()
 		elif(ch == 'a'):
 			client.getArticles()
 		elif(ch == 'p'):
--- RabbitMQ/server_a1.py
+++ RabbitMQ/server_a1.py
@@ -2,12 +2,49 @@
 import pika
 import uuid
 import server_pb2
+from datetime import date
+from datetime import datetime
 
+MAXCLIENTS = 10
+
+
+def isValidDate(date_str, format_str='%d %m %Y'):
+	try:
+		datetime.strptime(date_str, format_str)
+		return True
+	except ValueError:
+		print("VAL")
+		print(date_str)
+		return False
+
+def articleMatches(request, article):
+	# authors
+	if(request.author != "" and request.author != article.author):
+		return False
+	# date check
+	if(request.year > article.year):
+		return False
+	if(request.year == article.year and request.month > article.month):
+		return False
+	if(request.year == article.year and request.month == article.month and request.day > article.day):
+		return False
+	# check tag
+	if(request.allTags == True):
+		return True 
+	if(request.SPORTS != article.SPORTS):
+		return False
+	if(request.FASHION != article.FASHION):
+		return False
+	if(request.POLITICS != article.POLITICS):
+		return False
+	
+	return True
 
 class Server(object):
 	def __init__(self) -> None:
 		self.id = str(uuid.uuid4())
-
+		self.CLIENTELE = []
+		self.articles = []
 		self.connection = pika.BlockingConnection(
 			pika.ConnectionParameters(host="localhost")
 		)
@@ -42,30 +79,98 @@ class Server(object):
 		if props.type == "server-list":
 			print("got it!")
 
+	def joinServer(self, body):
+		if len(self.CLIENTELE) >= MAXCLIENTS:
+			return str("FAIL")
+		
+		client_detail = server_pb2.ClientDetails()
+		client_detail.ParseFromString(body)
+
+		self.CLIENTELE.append(client_detail.client_uuid)
+
+		return str("SUCCESS")
+
+	def leaveServer(self, body):
+		
+		client_detail = server_pb2.ClientDetails()
+		client_detail.ParseFromString(body)
+		
+		if client_detail.client_uuid in self.CLIENTELE:		
+			self.CLIENTELE = [client for client in self.CLIENTELE if client != client_detail.client_uuid]
+			return str("SUCCESS")
+		else:
+			return str("FAIL")
+
+	def getArticles(self, body):
+		request = server_pb2.Article()
+		request.ParseFromString(body) 
+
+		print(request)
+
+		response = server_pb2.ArticleList()
+		if (request.client_uuid not in self.CLIENTELE) or not (isValidDate(f"{request.day} {request.month} {request.year}")):
+			print(request.client_uuid not in self.CLIENTELE)
+			print(isValidDate(f"{request.day} {request.month} {request.year}"))
+			response.status = False
+		else:
+			response.status = True
+
+			for article in self.articles:
+				if articleMatches(body, article):
+					response.articles.append(article)
+
+		return response.SerializeToString()
+
+	def publishArticle(self, body):
+		request = server_pb2.Article()
+		request.ParseFromString(body) 
+
+		if (request.client_uuid not in self.CLIENTELE):
+			return str("FAIL")
+
+		print(f"ARTICLES PUBLISH FROM {request.client_uuid}")
+		request.year = date.today().year
+		request.month = date.today().month
+		request.day = date.today().day
+		content = request.content 
+		author = request.author 
+
+		emptyTopic = (request.SPORTS==False and request.FASHION==False and request.POLITICS==False)
+		if(content.strip() == "" or author.strip() == "" or emptyTopic or len(content) > 200):
+			return str("FAIL")
+	
+		self.articles.append(request)
+		print(request)
+		return str("SUCCESS")
+
 	
 	def handleRpc(self, ch, method, props, body):
 		print("Hello")
-		if props.type == "register":
-			print("register called")
-		elif props.type == "server-list":
-			print(f"server list requested from {props.user_id}")
+		if props.type == "join-server":
+			print(f"Join request from {props.correlation_id}")
+			response = self.joinServer(body)
+		elif props.type == "leave-server":
+			print(f"leave server requested from {props.correlation_id}")
+			response = self.leaveServer(body)
+		elif props.type == "get-articles":
+			print(f"get article requested from {props.correlation_id}")
+			response = self.getArticles(body)
+		elif props.type == "pub-article":
+			print(f"pub article requested from {props.correlation_id}")
+			response = self.publishArticle(body)
 
-		response = "hii"
+		
+
 
 		ch.basic_publish(
 			exchange='',
 			routing_key=props.reply_to,
 			properties=pika.BasicProperties(type=props.type),
-			body=str(response)
+			body=response
 		)
 
 		ch.basic_ack(delivery_tag=method.delivery_tag)
 		
-
-# message ServerDetails {
-#   string name = 1;
-#   string addr = 2;
-# }
 
 	def registerAsServer(self):
 		server_details = server_pb2.ServerDetails()
